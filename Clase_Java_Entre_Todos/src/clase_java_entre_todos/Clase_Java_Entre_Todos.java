18902f0a6735772eceec952dd5352fd7a2c5efcf
kiotoks <nikogr2005@gmail.com>
2023-04-26 08:34:42 -0300

    proyecto

---
+++ Clase_Java_Entre_Todos/src/clase_java_entre_todos/Clase_Java_Entre_Todos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package clase_java_entre_todos;
+
+/**
+ *
+ * @author ET36
+ */
+public class Clase_Java_Entre_Todos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Clase_Java_Entre_Todos/src/clase_java_entre_todos/main.java
@@ -0,0 +1,6 @@
+public static class main{
+    
+    Producto venta1[] = new Producto[]; 
+    Producto venta2[] = new Producto[];
+    Producto venta3[] = new Producto[];
+}
