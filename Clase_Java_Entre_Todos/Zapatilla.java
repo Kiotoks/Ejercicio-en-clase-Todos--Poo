a12a0f7fa81b363b3da4cb537c7631efddc33667
Quantoniumguy <116037194+Quantoniumguy@users.noreply.github.com>
2023-04-26 09:08:33 -0300

    Add files via upload

---
+++ Clase_Java_Entre_Todos/Zapatilla.java
@@ -0,0 +1,42 @@
+package zapatilla;
+/**
+ *
+ * Jeremias Leiva, Josue Bieber
+ */
+public class Zapatilla extends Producto{
+    int talle;
+    String uso;
+    
+    
+
+    public Zapatilla(String color, String desc, double pcio, String marca, int talle, String uso){
+        super(color, desc, pcio, marca, talle, uso);
+        this.talle = talle;
+        this.uso = uso;
+
+     }
+
+    public int getTalle() {
+        return talle;
+    }
+
+    public void setTalle(int talle) {
+        this.talle = talle;
+    }
+
+    public String getUso() {
+        return uso;
+    }
+
+    public void setUso(String uso) {
+        this.uso = uso;
+    }
+    
+    public String toString(){
+        String datos = super.toString() +" "+ talle +" "+ uso;
+    return datos;
+    
+   }
+
+}
+
